99576062130d4ef0012be887a349cda9f3b519e3
chikuuser <satyam93112@gmail.com>
2026-01-17 11:42:21 -0800

    aws new project-03

--- AWS/Project-03/terraform/terrafrom.tfvars
+++
@@ -1,2 +0,0 @@
-bucket-name = "aws-static-website-s3-cloudfront-edge-12345"
-project-name = "S3-Static-Website-Hosting-CloudFront-CDN"
--- AWS/Project-03/terraform/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-        source = "hashicrop/aws"
-        version = "~> 5.0"
-    }
-  }
-}
